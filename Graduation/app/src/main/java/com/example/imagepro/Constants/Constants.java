b3f86700871484c38f644de2a45e3b9fdc5eec9c
Yousef Mohamed <85323473+joemohamed2020@users.noreply.github.com>
2023-07-09 00:39:19 +0300

    Hosting

--- Graduation/app/src/main/java/com/example/imagepro/Constants/Constants.java
+++ Graduation/app/src/main/java/com/example/imagepro/Constants/Constants.java
@@ -1,7 +1,7 @@
 package com.example.imagepro.Constants;
 
 public class Constants {
-    public final static String rootIp = "http://192.168.1.7:80/signlator/";
+    public final static String rootIp = "https://signlator.000webhostapp.com/signlator/";
     public final static String login = rootIp+"login.php";
     public final static String register = rootIp+"signup.php";
     public final static String profile = rootIp+"profile.php";
